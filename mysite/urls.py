4f40af077f64e3b5b4d48a9d92d7b70475c4dba7
lauryndbrown <lauryndbrown@gmail.com>
2015-12-25 19:03:05 -0600

    Added the html for the views and change the url's to reflect it

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 		url(r'^$', views.index, name='index'),
-		url(r'^home/', views.index, name='home'),
-		url(r'^about/', views.about, name='about'),
-		url(r'^portfolio/', views.projects, name='projects'),
-		url(r'^blog/', views.blog, name='blog'),
-		url(r'^contact/', views.contact, name='contact'),
+		url(r'^index', views.index, name='home'),
+		url(r'^about', views.about, name='about'),
+		url(r'^projects', views.projects, name='projects'),
+		url(r'^blog', views.blog, name='blog'),
+		url(r'^contact', views.contact, name='contact'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Index")
+    context = []
+    return render(request, 'index.html', context)
 
 def about(request):
-	return HttpResponse("About")
+    context = []
+    return render(request, 'about.html', context)
 
 def projects(request):
-	return HttpResponse("Projects")
+    context = []
+    return render(request, 'projects.html', context)
 
 def blog(request):
-	return HttpResponse("Blog")
+    context = []
+    return render(request, 'blog.html', context)
 
 def contact(request):
-	return HttpResponse("Contact")
+    context = []
+    return render(request, 'contact.html', context)
