e3eba4dcd440da05e9ec3e8b91ff8441d73278f7
lauryndbrown <lauryndbrown@gmail.com>
2016-01-05 22:42:32 -0600

    Added detail to the blog admin page

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from mysite.models import BlogPost
-     
-admin.site.register(BlogPost)
+ 
+class BlogPostAdmin(admin.ModelAdmin):
+        list_display = ['title','sub_title']
+        list_filter = ['pub_date']
+        search_fields = ['title', 'sub_title', 'body']
+        date_hierarchy = 'pub_date'
+        save_on_top = True
+admin.site.register(BlogPost, BlogPostAdmin)
