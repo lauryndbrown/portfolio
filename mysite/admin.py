35502da06fa0a459d4a5ed7db43c6aa17e05034c
lauryndbrown <lauryndbrown@gmail.com>
2016-01-05 18:18:30 -0600

    Added blogpost to admin and added images to blog posts

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from mysite.models import BlogPost
+     
+admin.site.register(BlogPost)
--- mysite/models.py
+++ mysite/models.py
@@ -4,7 +4,9 @@ class BlogPost(models.Model):
    title = models.CharField(max_length = 60)
    sub_title = models.CharField(max_length = 200)
    pub_date = models.DateTimeField('date published')
+   image = models.ImageField(upload_to='blog')
    body = models.TextField()
+   
 
 class PostComment(models.Model):
    poster_name = models.CharField(max_length = 60)
