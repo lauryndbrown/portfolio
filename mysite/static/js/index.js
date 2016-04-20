95ed968af7716c15430b516e179350b6abea7f88
lauryndbrown <lauryndbrown@gmail.com>
2016-04-20 16:02:27 -0500

    Got the featurettes to appear when you scroll.

--- mysite/static/js/index.js
+++ mysite/static/js/index.js
@@ -1,7 +1,38 @@
 //copied from https://css-tricks.com/snippets/jquery/smooth-scrolling/
- //$("button").click(function() {
 $(document).on('click', 'button', function() {
     $('html,body').animate({
         scrollTop: $(".footer").offset().top},
         1000);
 });
+$(document).ready(function() {
+   
+  //hide objects 
+  $("#featurette1").hide();
+  $("#featurette2").hide();
+  $("#featurette3").hide();
+
+  //init scrolling event heandler
+  $(document).scroll(function(){
+   
+        var docScroll = $(document).scrollTop(); 
+       // var offset1 =  $("#featurette1").scrollTop() -100;
+        var offset1 =  $("#feature1").offset().top -500;
+        var offset2 =  $("#feature2").offset().top -500;
+        var offset3 =  $("#feature3").offset().top -500;
+ 
+    //when rich top of boxex than fire
+    if(docScroll > offset1 ) {
+
+      $("#featurette1").fadeIn(900)
+    
+    } if(docScroll > offset2 && docScroll < offset3){
+     $("#featurette2").fadeIn(900)
+    }
+     if(docScroll > offset3){
+     $("#featurette3").fadeIn(900)
+    }
+  })   
+});
+  
+  
+
